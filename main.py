be110d6a4024f58f3c5f9a13cf463994d2871ed1
Marty Naselli <marty@swiftbunny.com>
2024-01-15 15:13:21 -0500

    add odbc and caching

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
+import logging
+import pyodbc
+
 from fastapi import FastAPI, Form, Request, status
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
+
 import uvicorn
-
-
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    print('Request for index page received')
-    return templates.TemplateResponse('index.html', {"request": request})
+@app.get("/")
+@cache(namespace="run", expire=5)
+async def index():
+    logging.warning("RUNNING index()")
+    connection_string_parts = [
+        "DRIVER={Devart ODBC Driver for SQL Server}"
+        "Server="
+        "Database="
+        "Port="
+        "User ID="
+        "Password="
+    ]
+    connection = pyodbc.connect(";".join(connenction_string_parts))
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM table ORDER BY date LIMIT 1000")
+    return cursor.fetchall()
 
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
-
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+@app.on_event("startup")
+async def startup():
+    FastAPICache.init(InMemoryBackend())
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
-
